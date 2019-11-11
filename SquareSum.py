d7faf6cc311e3b7ced50d1630bf39e93f136190e
louisgallardo344 <57347181+louisgallardo344@users.noreply.github.com>
2019-11-11 22:45:11 +0800

    Add files via upload

---
+++ SquareSum.py
@@ -0,0 +1,6 @@
+n= input ("Enter a number: ")
+def squaresum(n) : 
+    return (n * (n + 1) * (2 * n + 1)) // 6
+
+n = 4
+print(squaresum(n)) 
---
+++ Vowel.py
@@ -0,0 +1,14 @@
+chr= ("Enter any letter:")
+
+if (chr=='a' or chr=='A' or chr=='e' or
+ chr=='E' or chr=='e' or  chr=='I' or chr=='i' or
+chr=='O' or chr=='U' or  chr=='u'):
+    print ("The alphaber is vowel", chr)
+
+else:
+   print("The letter is consonant", chr)
+
+
+
+
+
